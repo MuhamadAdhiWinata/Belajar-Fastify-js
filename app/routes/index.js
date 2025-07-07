5e50ab5b283cbb28c9937a06710df447982704b6
MuhamadAdhiWinata <muhamadadhiw@gmail.com>
2025-07-07 16:44:53 +0700

    feat: add routing

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,6 +7,42 @@ module.exports = fp(
         app: "Hello ini dari Fastify",
       };
     });
+
+    // GET: /users
+    appInstance.get("/user", {}, async function (request, replay) {
+      const data = [
+        {
+          nama: "adhinath",
+          email: "adhinath@example.com",
+        },
+        {
+          nama: "raiga",
+          email: "raiga@example.com",
+        },
+      ];
+      return replay.code(200).send({
+        status: "LOADED",
+        statusCode: 200,
+        message: "Data berahasil dimuat",
+        payload: {
+          data,
+        },
+      });
+    });
+
+    // POST: /users
+    appInstance.post("/user", {}, async function (request, replay) {
+      const user = request.body;
+      // const user = {};
+      return replay.code(201).send({
+        status: "CREATED",
+        statusCode: 201,
+        message: "Data berahasil dibuat",
+        payload: {
+          user,
+        },
+      });
+    });
   },
   { name: "app-routes" }
 );
